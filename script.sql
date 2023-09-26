9a5c73e5d909a173f77a31b891fb50516502b5e1
Lucas Silva <silva.pereira.92@gmail.com>
2023-09-25 23:20:02 -0300

    ap16(cursores-exercicios): exercicio 1.3

--- script.sql
+++ script.sql
@@ -46,3 +46,20 @@ BEGIN
     CLOSE cur_cursor;
 END;
 $$
+
+-- 1.3 Faça uma pesquisa sobre o anti-pattern chamado RBAR - Row By Agonizing Row. Explique com suas palavras do que se trata.
+
+-- RBAR (Row By Agonizing Row) é um anti-pattern que se refere a um método de processamento de dados em que cada linha ou registro
+-- de um conjunto de dados é processado de forma individual e sequencial. Isso significa que, em vez de aproveitar a capacidade de
+-- processamento em lote dos sistemas de banco de dados e realizar operações em conjuntos de dados, cada linha é processada uma de cada vez.
+
+-- Esse padrão é "agonizante" porque é ineficiente e geralmente leva a um desempenho ruim em operações que poderiam ser executadas
+-- muito mais rapidamente em um conjunto de dados inteiro. É como "agonizar" sobre cada linha, processando-as uma por uma, em vez 
+-- de realizar operações em massa.
+
+-- Exemplos comuns de RBAR incluem o uso inadequado de loops em SQL ou linguagens de programação para iterar sobre resultados de 
+-- consultas em vez de aproveitar as capacidades de conjunto de dados do banco de dados. Isso pode levar a consultas lentas e ineficientes
+-- , especialmente em grandes conjuntos de dados.
+
+-- A boa prática é evitar o RBAR sempre que possível e procurar maneiras de usar consultas SQL e operações em conjunto de dados para alcançar
+-- o mesmo resultado de forma mais eficiente. Isso geralmente envolve o uso de agregações, junções e outras técnicas de consulta em SQL.
